ea014c51cbfc7e494d49de01101f23a49fdb585c
aryooww <setyoaryowiguno@gmail.com>
2026-05-12 17:14:52 +0700

    task manager

---
+++ TaskManager.java
@@ -0,0 +1,7 @@
+  import java.util.ArrayList;
+  import java.util.List;
+
+  public class TaskManager{
+      // Data Structure to hold the objects
+      private List<Task> taskList = new ArrayList<>();
+  }
